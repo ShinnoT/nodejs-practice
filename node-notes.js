003fbb5361b6733db1273a52db6235e8cd58638f
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-12 02:07:21 +0900

    notes

---
+++ node-notes.js
@@ -0,0 +1,14 @@
+// Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+
+// node has file system manipulation
+// while browser manipulates whats shown inside the window
+
+// in browser:
+  // window--> global object that stores everything you have access to
+// on node:
+  // global not window because its not a browser
+
+// in browser:
+  // document --> references the DOM up above in the browser (HTML, CSS, etc.)
+// on node:
+  // process same thing as document on browser
