88055bc696f1b28da971b2aef5a3ab5eb48b81f9
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-12 16:24:30 +0900

    user fetched

--- second-app/app.js
+++ second-app/app.js
@@ -2,6 +2,7 @@ console.log("starting app");
 
 // load a built in module (library)
 const fs = require("fs");
+const os = require("os");
 // now you can use the fs (file system library)
 // which is stored in the variable fs
 
@@ -9,11 +10,24 @@ const fs = require("fs");
 // this is the classic way which will give an error
 
 // so you can do either:
-fs.appendFile('greetings.txt', 'Hello World!', function (err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+// fs.appendFile('greetings.txt', 'Hello World!', function (err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
 
 // or a more new way of doing it
 // fs.appendFileSync('greetings.txt', 'Hello World!');
+
+
+// when you run the app you can see the the greetings.txt file
+// has been created in the same directory of the app
+// if the file doesnt exit it makes it
+// if you run the app again, it will add Hello World! again
+
+
+// ------------------------
+// using userInfo from os module
+let user = os.userInfo();
+//userInfo doesnt need any arguments, they are optional
+console.log(user);
