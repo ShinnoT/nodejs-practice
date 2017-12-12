0088d909d9c88b9beb8ab585f6759653ba3f58b3
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-12 14:49:26 +0900

    v8 engine notes

--- node-notes.js
+++ node-notes.js
@@ -1,4 +1,6 @@
 // Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+// v8 Engine is a comiler that takes JS code and turns it into machine code
+
 
 // node has file system manipulation
 // while browser manipulates whats shown inside the window
