1c70b483b8135687b8576fee3913127bb281e88c
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-14 18:17:08 +0900

    parse and notes

--- second-app/playground/json.js
+++ second-app/playground/json.js
@@ -1,9 +1,18 @@
-let obj = {
-  name: "shinno"
-};
+// let obj = {
+//   name: "shinno"
+// };
 
-let stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-//will return string
-console.log(stringObj);
-//will return {"name":"shinno"}
+// let stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// //will return string
+// console.log(stringObj);
+// //will return {"name":"shinno"}
+
+
+let personString = '{"name": "shinno","age": 25}';
+let person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
+
+// so you use stringify to WRITE something to a JSON file
+// and you use parse to READ something from a JSON file by converting it to an object
