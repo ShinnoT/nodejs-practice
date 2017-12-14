c1d39518dda082ef78c34a69ca7e8e24dbebf1fe
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-14 18:29:26 +0900

    reading writing to json file

--- second-app/playground/json.js
+++ second-app/playground/json.js
@@ -9,10 +9,26 @@
 // //will return {"name":"shinno"}
 
 
-let personString = '{"name": "shinno","age": 25}';
-let person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// let personString = '{"name": "shinno","age": 25}';
+// let person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
 
 // so you use stringify to WRITE something to a JSON file
 // and you use parse to READ something from a JSON file by converting it to an object
+
+const fs = require('fs');
+
+let originalNote = {
+  title: 'some title',
+  body: 'some body'
+};
+
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
