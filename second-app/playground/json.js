0a63c73f10096c6aa54d0f5db5c776730fa6a812
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-14 18:12:08 +0900

    stringified obj for JSON

---
+++ second-app/playground/json.js
@@ -0,0 +1,9 @@
+let obj = {
+  name: "shinno"
+};
+
+let stringObj = JSON.stringify(obj);
+console.log(typeof stringObj);
+//will return string
+console.log(stringObj);
+//will return {"name":"shinno"}
