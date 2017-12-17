12028004951584c2cda6c3070a4cc1bc31624350
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-17 14:20:43 +0900

    debugging complete

---
+++ second-app/playground/debugging.js
@@ -0,0 +1,34 @@
+let person = {
+  name: 'Brad'
+};
+
+person.age = 25;
+
+debugger;
+// when you include this statement in your code
+// it will allow you to skip to these points with c
+
+person.name = 'Mike';
+
+console.log(person);
+
+
+// to debug begin by typing, on terminal:
+// node inspect filename.js
+
+// then you can do things like:
+// list(10) which lists 10 lines of code
+// n which stands for next goes through each line of code
+    // - note here that the line debugger is on is not executed yet
+// c which runs the rest of the code from where you are
+
+// repl <-- 're-evaluate print loop' (similar to console on developer tools)
+// this command will allow you to mess with your code from inside terminal
+// to check if things are going the way they should
+// not that if you change something, like reassign a value to a variable
+// in debugger it will change (not in real life code tho so if you exit debugger itll be back to normal)
+
+
+//note that you can use nodemon inspect blah
+
+// when testing a new feature gotta do node inspect app.js read --title="note 1"
